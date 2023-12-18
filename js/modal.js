d96e03fccb2d9d251397fe0335f9d97267d766de
John Idoko <idokojohn42@yahoo.com>
2023-12-18 16:44:02 +0100

    ADMIN: - Dashboard - Customer List - Transaction Request List

---
+++ js/modal.js
@@ -0,0 +1,55 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const openModalBtn = document.getElementById('openModalBtn');
+    const closeModalBtn = document.getElementById('closeModalBtn');
+    const modal = document.getElementById('myModal');
+  
+    openModalBtn.addEventListener('click', function () {
+      modal.style.display = 'block';
+    });
+  
+    closeModalBtn.addEventListener('click', function () {
+      modal.style.display = 'none';
+    });
+  
+    window.addEventListener('click', function (event) {
+      if (event.target === modal) {
+        modal.style.display = 'none';
+      }
+    });
+  });
+  
+
+  document.addEventListener('DOMContentLoaded', function () {
+    const toggleCheckbox = document.getElementById('toggleCheckbox');
+    const contentToToggle = document.getElementById('contentToToggle');
+  
+    toggleCheckbox.addEventListener('change', function () {
+      contentToToggle.style.display = toggleCheckbox.checked ? 'block' : 'none';
+    });
+  });
+
+  document.addEventListener('DOMContentLoaded', function () {
+    const approveCheckbox = document.getElementById('approveCheckbox');
+    const cancelCheckbox = document.getElementById('cancelCheckbox');
+    const contentToToggle = document.getElementById('contentToToggle');
+    const contentToToggleII = document.getElementById('contentToToggleII');
+  
+    approveCheckbox.addEventListener('change', function () {
+      if (approveCheckbox.checked) {
+        cancelCheckbox.checked = false;
+        // Additional logic or actions if needed
+        contentToToggle.style.display = 'block';
+        contentToToggleII.style.display = 'none';
+      }
+    });
+  
+    cancelCheckbox.addEventListener('change', function () {
+      if (cancelCheckbox.checked) {
+        approveCheckbox.checked = false;
+        // Additional logic or actions if needed
+        contentToToggle.style.display = 'none';
+        contentToToggleII.style.display = 'block';
+      }
+    });
+  });
+  
